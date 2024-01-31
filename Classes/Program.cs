a49fb15322a9a78423a3a496a0253131c2593f80
juliannegodsey1 <juliannegodseyy@gmail.com>
2024-01-31 11:21:40 -0500

    redo complete

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Focus";
+            myCar.Year = 2013;
+
+
+            Car mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CS-15",
+                Year = 2015
+            };
+
+            Car chevy = new Car("Chevy", "Impala", 2001);
+
         }
     }
 }
